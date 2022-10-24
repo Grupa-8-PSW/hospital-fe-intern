a8ba5e58419180f13b8ba9f0ac811aab7f14de42
Damjan <dimitrijevicdamjan40@gmail.com>
2022-10-24 21:06:07 +0200

    Layout and home page design
    Basic folder structure, basic layout and home page design

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from 'src/app/app-routing.module'; 
 import { HomeComponent } from './home/home.component';
+import { ExaminationsComponent } from './examinations/examinations.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    ExaminationsComponent,
   ],
   imports: [
     CommonModule,
