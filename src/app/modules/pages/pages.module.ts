36b991f67f554411ba06eb0b52ef8491d8e89f36
birdpersson <aleksandarvujasin@gmail.com>
2022-10-26 14:28:55 +0200

    Initial map

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ExaminationsComponent } from './examinations/examinations.component';
 import { MaterialModule } from 'src/app/material/material.module';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     ExaminationsComponent,
+    MapComponent,
   ],
   imports: [
     CommonModule,
