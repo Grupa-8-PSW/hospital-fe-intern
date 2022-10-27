2acfbea3fb49ef0db1a402ca267adaae36fd37d2
spiricnra2052019 <spiricn00@gmaile.com>
2022-10-27 14:55:45 +0200

    AddedBasics

---
+++ src/app/modules/map/map.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './map.component.html',
+    styleUrls: ['./map.component.css']
+})
+export class AppComponent {
+    title = 'RoomsByFabric';
+    state = false;
+    numberofRoom = "";
+    parentFunction(data) {
+        this.state = data;
+        console.log(data);
+    }
+
+    getNumberOfRoom(roomNum) {
+        this.numberofRoom = roomNum;
+    }
+}
--- src/app/modules/map/map.module.ts
+++ src/app/modules/map/map.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormComponent } from './floor/form/form.component';
-import { RoomsComponent } from './floor/rooms/rooms.component';
-
+import { FormComponent } from './form/form.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 
 @NgModule({
