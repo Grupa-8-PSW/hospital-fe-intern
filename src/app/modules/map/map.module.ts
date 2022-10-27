47b634053299f90e418e4feeb7b8d464d66b0047
birdpersson <aleksandarvujasin@gmail.com>
2022-10-27 20:58:23 +0200

    Initial libraries

--- src/app/modules/map/map.module.ts
+++ src/app/modules/map/map.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HospitalComponent } from './hospital/hospital.component';
+import { BuildingComponent } from './building/building.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HospitalComponent,
+    BuildingComponent
+  ],
   imports: [
     CommonModule
   ]
