06f5cf0f6a32b7c8a55aa5c2110178e42b4f933d
Darko Filipovic <darkofilipovic1010@gmail.com>
2022-11-02 23:41:05 +0100

    feedback displaying feature implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { FeedbackDisplayComponent } from "./modules/hospital/feedback/feedback-display/feedback-display.component";
 import { ExaminationsComponent } from "./modules/pages/examinations/examinations.component";
 import { HomeComponent } from "./modules/pages/home/home.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'examinations', component: ExaminationsComponent }
+  { path: 'examinations', component: ExaminationsComponent },
+  {path: 'feedback/display', component : FeedbackDisplayComponent}
 ];
 
 @NgModule({
--- src/app/modules/hospital/feedback/feedback-display/feedback-display.component.ts
+++ src/app/modules/hospital/feedback/feedback-display/feedback-display.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Feedback } from '../model/feedback.model';
+import { FeedbackService } from '../services/feedback.service';
 
 @Component({
   selector: 'app-feedback-display',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackDisplayComponent implements OnInit {
 
-  constructor() { }
+  public Feedbacks: Feedback[] = [];
+
+  constructor(private feedbackService: FeedbackService) { }
 
   ngOnInit(): void {
+    this.feedbackService.getAllFeedbacks().subscribe(response=>{
+      this.Feedbacks=response;
+    })
   }
 
 }
