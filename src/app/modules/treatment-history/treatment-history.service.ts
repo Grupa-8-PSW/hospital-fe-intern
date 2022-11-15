a16595f11e8b4fa9bfee228d4a81668a4362d43f
Fikus00 <filipnikolic0000@gmail.com>
2022-11-15 23:11:09 +0100

    create treatment history service

--- src/app/modules/treatment-history/treatment-history.service.ts
+++ src/app/modules/treatment-history/treatment-history.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import * as moment from 'moment';
+import TreatmentHistory from 'src/app/model/treatmentHistory';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TreatmentHistoryService {
 
-  constructor() { }
+  treatmentHistoryUrl = `${environment.apiUrL}/TreatmentHistory/`;
+  headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllTreatmentHistorys() : Observable<TreatmentHistory[]> {
+    return this.http.get<TreatmentHistory[]>(this.treatmentHistoryUrl);
+  }
+
+  getTreatmentHistoryById(id: number): Observable<TreatmentHistory> {
+    return this.http.get<TreatmentHistory>(this.treatmentHistoryUrl + id).pipe(
+      map(treatmentHistory => {
+        return {
+          ...treatmentHistory,
+          startDate: moment(treatmentHistory.startDate),
+          endDate: moment(treatmentHistory.endDate)
+        }
+      })
+    );
+  }
+
+  createTreatmentHistory(treatmentHistory: TreatmentHistory): Observable<any> {
+    return this.http.post(this.treatmentHistoryUrl, treatmentHistory, { headers: this.headers });
+  }
+
+  
 }
