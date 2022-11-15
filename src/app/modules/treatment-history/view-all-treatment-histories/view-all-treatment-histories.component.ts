1422ea92d61374f06c056764648d627962c4be3a
Fikus00 <filipnikolic0000@gmail.com>
2022-11-15 21:09:21 +0100

    added base components

---
+++ src/app/modules/treatment-history/view-all-treatment-histories/view-all-treatment-histories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-treatment-histories',
+  templateUrl: './view-all-treatment-histories.component.html',
+  styleUrls: ['./view-all-treatment-histories.component.css']
+})
+export class ViewAllTreatmentHistoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
