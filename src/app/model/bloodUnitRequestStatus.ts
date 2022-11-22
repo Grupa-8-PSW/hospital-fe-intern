0d43275007f877d32cd05cc6469bf6676e0eda10
unknown <dzonik125@gmail.com>
2022-11-22 14:01:52 +0100

    Implemented view for all blood requests

---
+++ src/app/model/bloodUnitRequestStatus.ts
@@ -0,0 +1,5 @@
+export enum BloodUnitRequestStatus{
+    REJECTED,
+    APPROVED,
+    WAITING
+}
---
+++ src/app/modules/pages/bloodBanks/view-blood-requests/view-blood-requests.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { BloodUnitRequest } from 'src/app/model/bloodUnitRequest';
+import { BloodRequestServiceService } from '../services/blood-request-service.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { BloodTypePipe } from '../pipes/blood-type.pipe';
+
+@Component({
+  selector: 'app-view-blood-requests',
+  templateUrl: './view-blood-requests.component.html',
+  styleUrls: ['./view-blood-requests.component.css']
+})
+export class ViewBloodRequestsComponent {
+
+  requests: BloodUnitRequest[] = [];
+  displayedColumns: string[] = ['id', 'amountL', 'type', 'reason', 'creationDate', 'approve', 'reject'];
+
+  dataSource = new MatTableDataSource(this.requests);
+
+  constructor(private service: BloodRequestServiceService) { }
+
+  ngOnInit(): void {
+    this.service.getBloodRequests().subscribe(res => {
+      this.requests = res;
+      console.log(this.requests);
+    })
+  }
+
+}
