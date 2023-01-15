27a6bc15b17b9920d8dbe6b5fdeadd5161a5d2ec
zinda12 <zin00aleksa@gmail.com>
2023-01-15 17:42:28 +0100

    bug fix

--- src/app/model/appointment-event-statistic.model.ts
+++ src/app/model/appointment-event-statistic.model.ts
@@ -1,7 +1,11 @@
 import AppointmentStepViewCountStatistic from "./appointment-step-view-count-statistic.model";
+import PatientAgeStatistic from "./patient-age-statistic.model";
 
 export default interface AppointmentEventStatistic {
     averageNumberOfStep: number;
     averageSecondsOfScheduling: number;
     stepViewCountStatistic: AppointmentStepViewCountStatistic;
+    durationViewingEachStep: AppointmentStepViewCountStatistic;
+    succesfullPercentage: number;
+    ageStatistic: PatientAgeStatistic;
 }
